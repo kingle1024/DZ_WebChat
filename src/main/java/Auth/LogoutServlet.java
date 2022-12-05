8d9da7796c1c819ebee33a5d620cf701d5a3997e
kingle1024 <teran1024@naver.com>
2022-12-05 15:36:05 +0900

    로그아웃 구현

---
+++ src/main/java/Auth/LogoutServlet.java
@@ -0,0 +1,37 @@
+package Auth;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "logout", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        doHandle(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        doHandle(request, response);
+    }
+
+    private void doHandle(HttpServletRequest request, HttpServletResponse response){
+
+        HttpSession session = request.getSession();
+        session.invalidate();
+        RequestDispatcher dispatch;
+
+        dispatch = request.getRequestDispatcher("/login.jsp");
+        try {
+            dispatch.forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
