1b407e5d60ee880c2cadbf7d26f3ba4fde07a5c2
kingle1024 <teran1024@naver.com>
2022-12-05 15:35:24 +0900

    로그인 세션 구현

---
+++ src/main/java/Filter/EncoderFilter.java
@@ -0,0 +1,75 @@
+package Filter;
+
+import java.util.HashSet;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Set;
+
+@WebFilter(
+        urlPatterns = "/*",
+        initParams = {
+                @WebInitParam(name="no.auth.urls", value="login.jsp"),
+                @WebInitParam(name="no.auth.urls", value="/login"),
+                @WebInitParam(name="login.url", value="/login.jsp"),
+                @WebInitParam(name="encoding", value="utf-8")
+        }
+)
+public class EncoderFilter implements Filter {
+
+    ServletContext context;
+    String loginUrl = "";
+    Set<String> authUrls = new HashSet<>();
+
+    public void init(FilterConfig fConfig) {
+        System.out.println("EncodingFilter.init() : utf-8 Encoding.....");
+        context = fConfig.getServletContext();
+        String context = fConfig.getServletContext().getContextPath();
+        loginUrl = context + fConfig.getInitParameter("login.url");
+
+        authUrls.add("/login");
+        authUrls.add("/login.jsp");
+        authUrls.add("/register.jsp");
+        authUrls.add("/register.html");
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 서블릿보다 먼저 호출됨
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+
+        String context = ((HttpServletRequest) request).getContextPath();
+        String pathInfo = ((HttpServletRequest) request).getRequestURI();
+        String realPath = request.getRealPath(pathInfo);
+        String message = "Context Info :"+context +
+                "\n URI Info : "+ pathInfo +
+                "\n Physical Path : " + realPath;
+//        System.out.println(message+"\n");
+
+        /*로그인 된 사용자만 사용 가능한 URL 인지 확인*/
+        if(isAuth(req, pathInfo)) {
+            chain.doFilter(request, response);
+//            System.out.println("작업 시간 : " + (end - begin) + "ms");
+        }else {
+            System.out.println("loginUrl :"+loginUrl);
+            resp.sendRedirect(loginUrl);
+        }
+    }
+    private boolean isAuth(HttpServletRequest req, String pathInfo) {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("isLogon");
+
+        boolean isLogin = value != null ? (Boolean) value : false;
+//        authUrls = (Set<String>) context.getAttribute("authUrls");
+
+        return (isLogin || authUrls.contains(pathInfo));
+    }
+}
