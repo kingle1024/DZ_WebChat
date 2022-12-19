32c8b5d98ea8a4513e9498ee0bb02b1bed234108
kingle1024 <teran1024@naver.com>
2022-12-19 16:45:41 +0900

    페이징 <, <<, >, >> 추가

--- src/main/java/Page/PageUtil.java
+++ src/main/java/Page/PageUtil.java
@@ -2,11 +2,9 @@ package Page;
 
 public class PageUtil {
     // 전체 개수
-    private long totalCount;
+    private final long totalCount;
     // 한 페이지에 나오는 개수
-    private long pageSize = 10;
-    // 페이지블럭 개수
-    private long pageBlockSize = 10;
+    private long pageSize;
     // 현재 페이지 번호
     private long pageIndex;
     // 시작페이지
@@ -30,13 +28,25 @@ public class PageUtil {
         if(queryString != null && queryString.length() > 0){
             addQueryString = "&" + queryString;
         }
+        // 이전 페이지 처리
+        if(startPage != pageIndex) {
+            sb.append(String.format("<a href='?pageIndex=%d%s'> &lt;&lt;</a>", 1, addQueryString));
+            sb.append("&nbsp;");
+            sb.append(String.format("<a href='?pageIndex=%d%s'> &lt; </a>", pageIndex-1, addQueryString));
+        }
 
         for(long i = startPage; i<= endPage; i++){
             if(i == pageIndex)
-                sb.append(String.format("<a class='on' href='?pageIndex=%d%s'>%d</a>", i, addQueryString, i));
+                sb.append(String.format("<span style='font-size:1.3rem;'> %d </span>", i));
             else
                 sb.append(String.format("<a href='?pageIndex=%d%s'>%d</a>", i, addQueryString, i));
             sb.append(System.lineSeparator());
+        }
+
+        if(endPage != pageIndex){
+            sb.append(String.format("<a href='?pageIndex=%d%s'> &gt; </a>", pageIndex+1, addQueryString));
+            sb.append("&nbsp;");
+            sb.append(String.format("<a href='?pageIndex=%d%s'> &gt;&gt;</a>", endPage, addQueryString));
         }
 
         sb.append(System.lineSeparator());
@@ -53,6 +63,8 @@ public class PageUtil {
         }
         // 전체페이지 블럭 개수
         long totalBlockCount = totalCount / pageSize + (totalCount % pageSize > 0 ? 1 : 0);
+        // 페이지블럭 개수
+        final long pageBlockSize = 10;
         startPage = ((pageIndex - 1) / pageBlockSize) * pageBlockSize + 1;
         endPage = startPage + pageBlockSize - 1;
 
