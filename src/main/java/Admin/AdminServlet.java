bd4e1dd5e12f75227b89882cbc702771357061b3
kingle1024 <teran1024@naver.com>
2022-12-19 16:33:26 +0900

    admin servlet 추가

---
+++ src/main/java/Admin/AdminServlet.java
@@ -0,0 +1,53 @@
+package Admin;
+
+import Config.CustomCofig;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "AdminServlet", value = "/admin/*")
+public class AdminServlet extends HttpServlet {
+    boolean isAdmin;
+    Map<String, Object> className2ObjectMap = new HashMap<>();
+    Map<String, Object> objectMap = new HashMap<>();
+    Map<String, Method> methodMap = new HashMap<>();
+    CustomCofig customCofig;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        customCofig = new CustomCofig();
+        customCofig.initAction("GET", this.getClass(), className2ObjectMap, objectMap, methodMap);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        customCofig.action(request, response, objectMap, methodMap);
+    }
+
+    private void validAdmin(HttpServletRequest request, HttpServletResponse response) {
+        isAdmin = (boolean) request.getSession().getAttribute("login_admin");
+        if(!isAdmin) {
+            try {
+                response.sendRedirect("/");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("AdminServlet doPost > ");
+
+    }
+}
